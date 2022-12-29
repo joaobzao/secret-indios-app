13efb0079ba326bf031014c84fdeae7d38209a96
zaoj <joao.zao@blip.pt>
2022-12-29 19:11:24 +0000

    create dummy groups list

--- ios/KaMPKitiOS/SecretSantaApp.swift
+++ ios/KaMPKitiOS/SecretSantaApp.swift
@@ -21,7 +21,7 @@ struct SecretSantaApp: App {
     
     var body: some Scene {
         WindowGroup {
-            BreedListScreen()
+            GroupsView()
         }
     }
 }
---
+++ ios/KaMPKitiOS/Views/GroupsView.swift
@@ -0,0 +1,66 @@
+//
+//  GroupsView.swift
+//  KaMPKitiOS
+//
+//  Created by Joao Zao on 29/12/2022.
+//  Copyright © 2022 Touchlab. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupsView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section("Groups Accepted") {
+                    ForEach(1...6, id: \.self) { _ in
+                        GroupItemView()
+                            .listRowSeparator(.hidden)
+                    }
+                }
+                
+                Section("Invites pending") {
+                    ForEach(1...3, id: \.self) { _ in
+                        GroupItemView()
+                            .listRowSeparator(.hidden)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .bottom, content: {
+                Button("Create Group") {
+                    print("🎅 Create button tap")
+                }
+                .frame(maxWidth: .infinity, maxHeight: 70)
+                .foregroundColor(Color.white)
+                .background(Color("AccentGreen"))
+                .cornerRadius(12)
+                .padding()
+            })
+            .navigationTitle("Santa Groups")
+            .listStyle(.sidebar)
+            .listSectionSeparatorTint(Color("AccentGreen"))
+            .scrollContentBackground(.hidden)
+        }
+    }
+}
+
+struct GroupItemView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "brain.head.profile")
+                .foregroundColor(Color.green)
+                .frame(width: 50, height: 50)
+                .background(Color("LightGreen"))
+                .cornerRadius(12)
+            Text("John 🎅")
+            
+            Spacer()
+        }
+    }
+}
+
+struct GroupsView_Preview: PreviewProvider {
+    static var previews: some View {
+            GroupsView()
+    }
+}
